408316a2414d92cb6ffe32baeea02b94887e8ec1
DANGO <dahianaprogramacion@gmail.com>
2024-07-05 12:53:23 -0300

    subiendo arreglos de js

---
+++ Js/bar.js
@@ -0,0 +1,14 @@
+//*menu hamburguesa*//
+const nav = document.querySelector(".nav-menu-item");
+const abrir = document.querySelector(".abrir");
+const cerrar = document.querySelector(".cerrar");
+
+abrir.addEventListener("click", ()=>{
+   getElementsByClassName
+    .add("visible");
+}) 
+
+cerrar.addEventListener("click", ()=>{
+    nav.classList.remove("visible");
+}) 
+
